fce45115f751a5f78c9b52abc0e47d0ce305976e
Uriel Loreto <uriel.loreto8@gmail.com>
2021-04-30 16:02:17 -0500

    configuracion redux

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+
+import { combineReducers } from "redux";
+import appointments from "./appointments";
+
+export default combineReducers({
+  appointments: appointments
+});
---
+++ src/utils/types.js
@@ -0,0 +1,6 @@
+const types = {
+  ADD_APPOITMENTS: "ADD_APPOITMENTS",
+  RESET_ALL_DATA: 'RESET_ALL_DATA',
+};
+
+export default types;
